c3c99331aba05569f4f947e3d8af56f0b3758c8a
ktapia576 <ktapia576@hotmail.com>
2025-02-28 06:13:12 -0500

    update comment

--- app/components/button.js
+++ app/components/button.js
@@ -1,6 +1,13 @@
 
-/* Button component to reuse same Tailwind Styling throughout project. 
-This makes code neater and less verbose for parent Button wrapper components*/
+/* 
+
+Button component to reuse same Tailwind Styling throughout project. 
+This makes code neater and less verbose for parent Button wrapper components.
+Still allows customization from parent component due to className being passed 
+down as prop and appended to end of className attribute, taking precedence over
+parent styling if needed 
+
+*/
 
 export default function Button({children, className = "", ...props}) {
     return (
