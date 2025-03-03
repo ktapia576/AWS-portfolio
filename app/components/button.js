27d4f3db132106e5de9e215d48b30d3600e813f8
ktapia576 <ktapia576@hotmail.com>
2025-03-02 20:58:19 -0500

    Add DaisyUI to project and change custom theme names

--- app/components/button.js
+++ app/components/button.js
@@ -12,7 +12,7 @@ parent styling if needed
 export default function Button({children, className = "", ...props}) {
     return (
         <button 
-            className={`bg-primary-light text-white outline-2 outline-primary-outline px-4 py-2 rounded  hover:bg-secondary cursor-pointer ${className}`}
+            className={`bg-custom-primary-light text-white outline-2 outline-custom-primary-outline px-4 py-2 rounded hover:bg-custom-secondary cursor-pointer ${className}`}
             {...props}
         >
             {children}
--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className=" bg-primary">{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
