f93232a50896b77a4b1d2d4dec40a886ed58746d
ktapia576 <ktapia576@hotmail.com>
2025-03-02 21:16:33 -0500

    Add DaisyUI components and customize with tailwindcss utlitily css

--- app/page.js
+++ app/page.js
@@ -5,16 +5,20 @@ export default function HomePage() {
     const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
     
     return (
-        <div className="grid place-items-center gap-5 text-white">
-            {/* Nesting the Header component */}
-            <Header className="pt-20 pb-10" title="Hello! My name is Kevin Tapia"/>
-            <Header title="I enjoy diving deep into my learning and expanding my skillset. I love Computer Science!"/>
-            <ul>
-                {names.map((name) => (
-                    <li key={name}>{name}</li>
-                    ))}
-            </ul>
-            <LikeButton />
+        <div className="hero min-h-screen">
+            <div className="hero-content text-center">
+                <div className="max-w-lg">
+                    <Header className="text-5xl font-bold" title="Hello there! My name is Kevin Tapia" />
+                    <Header className="py-6" title="I enjoy diving deep into my learning and expanding my skillset. I love Computer Science!"/>
+                    <ul>
+                        {names.map((name) => (
+                            <li key={name}>{name}</li>
+                            ))}
+                    </ul>
+                    <LikeButton />
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
         </div>
     );
 }
